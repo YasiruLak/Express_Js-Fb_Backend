01f585a0293eda2b9966a9c35f177e387126f533
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-08-21 22:30:49 +0530

    User CRUD Done

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-
-const item = require('./routes/user')
+const user = require('./routes/user')
 
 const app = express()
 const port = 4000
@@ -19,12 +18,6 @@ con.on("open", () => {
 app.use(express.json())
 
 app.use('/user', user)
-
-
-// app.get('/', (req, res) => {
-//     console.log('get request comming!');
-//     res.send('get req came for / route')
-// })
 
 app.listen(port, () => {
   console.log(`app starting on ${port}`);

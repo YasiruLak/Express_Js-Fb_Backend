5455dee0394655658606923cca16f71fcc780679
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-08-21 22:52:52 +0530

    Create Post CRUD Done

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const user = require('./routes/user')
+const posts = require('./routes/post')
+const login = require('./routes/login')
 
 const app = express()
 const port = 4000
@@ -18,6 +20,8 @@ con.on("open", () => {
 app.use(express.json())
 
 app.use('/user', user)
+app.use('/post', posts)
+app.use('/login', login)
 
 app.listen(port, () => {
   console.log(`app starting on ${port}`);
--- routes/post.js
+++ routes/post.js
@@ -4,9 +4,71 @@ const router = express.Router()
 
 const Post = require('../models/post.model')
 
+
 app.use(express.json())
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.json(posts)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
 
+router.get('/:id', async (req, res) => {
+    try {
+        const posts = await Post.findById(req.params.id)
+        res.json(posts)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+router.post('/', async (req, res) => {
+    const posts = new Post({
+        userId: req.body.userId,
+        date: req.body.date,
+        time: req.body.time,
+        title: req.body.title,
+        body: req.body.body
+    })
+
+    try {
+        const response = await posts.save()
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const posts = await Post.findById(req.params.id)
+        const response = await posts.remove()
+
+        res.json(response)
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+        const posts = await Post.findById(req.params.id)
+        posts.userId = req.body.userId
+        posts.date = req.body.date
+        posts.time = req.body.time
+        posts.title = req.body.title
+        posts.body = req.body.body
+
+        const response = await posts.save()
+        res.json(response)
+
+    } catch (err) {
+        res.send('Err: ' + err)
+    }
+})
 
 
 
